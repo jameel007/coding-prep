c18e2737b76634587ee70cd101812cdf877cfb93
jameel007 <33717591+jameel007@users.noreply.github.com>
2026-09-24 00:36:11 +0530

    Add files via upload

---
+++ excersizes.py
@@ -0,0 +1,18 @@
+## SQUARE OF n 
+"""Function to  return a square pattern of '*' of side n as a list of strings.
+    
+         Parameters:
+        n (int): The size of the square.
+    
+        Returns:
+        list: A list of strings where each string represents a row of the square.
+"""
+    # Your code here
+def generate_square(n):
+    s=[n]
+    for i in s:   
+        return s[i]
+        ##return s[i]
+    s[0]+=s[n].get(n,0)
+    print(s[2])    
+generate_square(3)
